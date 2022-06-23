3440db3814e1395780d8207ce7adb461dfccb46b
sharo-jef <cleared.for.approach.runway23@gmail.com>
2022-06-23 17:33:08 +0900

    deleted: comment

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pwgen",
 	Short: "Generate password.",
@@ -38,8 +37,6 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
